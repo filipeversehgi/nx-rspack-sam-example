a65b0df1ee79302494a6b069fdbc66fdff15900a
Filipe Versehgi <filipeversehgi@gmail.com>
2024-11-05 11:54:35 -0300

    feat: first commit

---
+++ apps/lambda-hello/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'lambda-hello',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/lambda-hello',
+};
